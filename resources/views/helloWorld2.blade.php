cca060c13e92d31fb9fd1e6f9284a4cdf1d994b6
AnthonyBertottiFavero <bertottianthony1@gmail.com>
2023-06-24 14:26:28 +0200

    first commit

---
+++ resources/views/helloWorld2.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">Questo è il secondo Hello World di Laravel!</h1>
+    <p style="text-align: center; font-size:30px;">Ecco il paese più bello del Mondo!</p>
+    <img src="https://turismo.puglia.it/wp-content/uploads/sites/124/monopoli-hd.jpg" alt="Il posto più bello del mondo">
+    <a href="/" style="text-align: center;">Torna Indietro :( </a>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1 style="text-align: center;">Questo è il primo Hello World di Laravel!</h1>
+    <img src="https://yt3.googleusercontent.com/UXFC9eFKxjbOcUeEiIFqpywdInXJijIrh5bkfUMPlKhCLKaO6rrTfh5O5IzNTS_2Wap2yBk1J44=s900-c-k-c0x00ffffff-no-rj" alt="Boolean" style="height: 400px;">
+    <a href="/helloWorld2" style="display: block; text-align: center; font-size:50px; padding-top: 2rem">Clicca qui se vuoi vedere il paese più bello del Mondo!</a>
+</body>
+
+</html>
